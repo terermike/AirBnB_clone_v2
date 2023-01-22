3b514be91259f318a85909e7f36fab9684a291d0
Mike <terermike3@gmail.com>
2023-01-22 21:47:06 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/""
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
               .format(filename))
 
         return "versions/web_static_{}.tgz".format(filename)
